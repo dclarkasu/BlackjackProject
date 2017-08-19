8233021b508543ba6e16b7e260cd93d4333aecf1
dclarkasu <dclark205@gmail.com>
2017-08-19 16:16:20 -0600

    Divide hit/stay and hand value code into methods. Added dealer

--- src/com/danielclark/blackjack/GameApp.java
+++ src/com/danielclark/blackjack/GameApp.java
@@ -1,5 +1,7 @@
 package com.danielclark.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // Dealer ArrayList. .add to give him a card. .get to get his cards
@@ -7,14 +9,15 @@ import java.util.Scanner;
 public class GameApp {
 	private Player p = new Player();
 	private Deck deck = new Deck();
+	private List<Card> dealer = new ArrayList<>();
 
 	public static void main(String[] args) {
 		GameApp game = new GameApp();
 		Scanner kb = new Scanner(System.in);
 		
 		game.welcome(kb);
-		int playerHandValue = game.run();
-		game.hitOrStay(kb, playerHandValue);
+		game.run();
+		game.hitOrStay(kb);
 		
 	}
 	
@@ -32,22 +35,32 @@ public class GameApp {
 		}
 	}
 	
-	public int run() {
+	public void run() {
 		deck = initializeDeck();
 		for (Card c : deck.getCards()) {
 //		System.out.println(c.getR() + " of " + c.getS() + " " + c.getR().getValue()); // prints out whole deck
 		}
 		p.getHand().addCard(deck.dealCard());
 		p.getHand().addCard(deck.dealCard());
+		dealer.add(deck.dealCard());
+		dealer.add(deck.dealCard());
 //		System.out.println(deck.getCards().size());
 		
-		int playerHandValue = 0;
-		for (Card c : p.getHand().getCards()) { // printing out the player's hand. Have to not only get Hand object but his actual cards
-			System.out.println(c);
-			playerHandValue = playerHandValue + c.getR().getValue();
-		}
+		int playerHandValue = valueOfPlayerHand();
+		int dealerHandValue = valueOfDealerHand();
+//		for (Card c : p.getHand().getCards()) { // printing out the player's hand. Have to not only get Hand object but his actual cards
+//			System.out.println(c);
+//			playerHandValue = playerHandValue + c.getR().getValue();
+//		}
+		
+		showPlayerHand();
+		showDealerHand("ONE");
+		
 		System.out.println("Player's Hand: " + playerHandValue);
-		return playerHandValue;
+		if(playerHandValue == 21) {
+			
+		}
+//		return playerHandValue;
 	}
 	
 	public Deck initializeDeck() {  // create deck first so we have somewhere to store Cards next
@@ -62,7 +75,9 @@ public class GameApp {
 		return deck;
 	}
 	
-	public void hitOrStay(Scanner kb, int playerHandValue) {
+	public void hitOrStay(Scanner kb) {
+		// Need to add actions for getting blackjack or busting
+		int playerHandValue = valueOfPlayerHand();
 		String choice;
 		do {
 		System.out.println("Would you like to hit or stay?");
@@ -72,12 +87,63 @@ public class GameApp {
 			System.out.println(c);
 				playerHandValue = playerHandValue + c.getR().getValue();
 			System.out.println("Player's Hand: " + playerHandValue);
+			System.out.println(deck.getCards().size()); // prints size of deck after each hit
 		}
 		if (choice.equalsIgnoreCase("stay")) {
 			// playerHandValue will stay the same
 			// turn will change	
 		}
 		} while(choice.equalsIgnoreCase("hit"));
+		
+		//while the dealers value is <= 17
+	}
+	
+//	public void dealerAction() {
+//		dealer
+//		
+//	}
+	
+	public void checkHandValue(int playerHandValue) {
+		if(playerHandValue > 21) {
+			System.out.println("You have busted. Game over");
+		}
+		if(playerHandValue == 21) {
+			System.out.println("Congratulations! You won.");
+			//add money to player wallet
+		}
+	}
+	
+	public void showPlayerHand() {
+		for (Card card : p.getHand().getCards()) {
+			System.out.println(card);
+		}
+	}
+	
+	public void showDealerHand(String amount) {
+		if(amount.equals("ONE")) {
+			System.out.println(dealer.get(0));
+		}
+		else if(amount.endsWith("ALL")) {
+			for (Card card : dealer) {
+				System.out.println(card);
+			}
+		}
+	}
+	
+	public int valueOfPlayerHand() {
+		int val = 0;
+		for (Card card : p.getHand().getCards()) {
+			val = val + card.getR().getValue();
+		}
+		return val;
+	}
+	
+	public int valueOfDealerHand() {
+		int val = 0;
+		for (Card card : dealer) {
+			val = val + card.getR().getValue();
+		}
+		return val;
 	}
 
 }
