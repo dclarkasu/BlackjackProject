caa596a6651559579326b389a0abb547ad803cf9
dclarkasu <dclark205@gmail.com>
2017-08-18 18:41:20 -0600

    Rank and more methods

--- src/com/danielclark/blackjack/Rank.java
+++ src/com/danielclark/blackjack/Rank.java
@@ -1,7 +1,8 @@
 package com.danielclark.blackjack;
 
 public enum Rank {
-	TWO(2);
+	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), 
+	KING(13), ACE(11);
 
 	int value;
 
